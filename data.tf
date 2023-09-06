76bd303908355f6e08873a2b0b8a3abc64104185
John Spinella <jrspinella@users.noreply.github.com>
2023-09-06 10:17:46 -0400

    Add hub data lookup

--- data.tf
+++ data.tf
@@ -8,3 +8,9 @@ data "azurerm_resource_group" "netwatch" {
   depends_on = [azurerm_virtual_network.spoke_vnet]
   name       = "NetworkWatcherRG"
 }
+
+data "azurerm_virtual_network" "hub_vnet" {
+  provider            = azurerm.hub_network
+  name                = var.hub_virtual_network_name
+  resource_group_name = var.hub_resource_group_name
+}
